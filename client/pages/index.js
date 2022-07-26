53afbb05c26ddf8e4ef09e5c8ec05b125fe310ce
zwang99 <zwangcq@gmail.com>
2022-07-26 00:28:33 -0700

    updates

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 const LandingPage = ({ currentUser, tickets }) => {
-  console.log("Running");
+  console.log("Running...");
   const ticketList = tickets.map((ticket) => {
     return (
       <tr key={ticket.id}>
