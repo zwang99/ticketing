e81e26899f0bf9cbd28b91df989afe8f2ffa755d
zwang99 <zwangcq@gmail.com>
2022-07-25 22:37:17 -0700

    update hostname

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import 'express-async-errors';
+import "express-async-errors";
 import { json } from "body-parser";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -9,23 +9,21 @@ import { errorHandler, NotFoundError } from "@wzhticketing/common";
 import cookieSession from "cookie-session";
 
 const app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test'
+    secure: false,
   })
 );
-
 
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-
-app.all('*', async (req, res) => {
+app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
 
--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,7 @@ export default ({ req }) => {
   if (typeof window === "undefined") {
     // we are on the server
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://ticketing-app.lol/",
       headers: req.headers,
     });
   } else {
